5157eb3dfad4918bb013569a9f9c4f9a8e75e6da
Rugtveit <BenjaminRugtveit@gmail.com>
2020-10-01 09:54:23 +0200

    Going back to recreating 1:1 port

--- src/main.cpp
+++ src/main.cpp
@@ -16,25 +16,23 @@ const Logger& getLogger() {
   static const Logger& logger(modInfo);
   return logger;
 }
-std::string buttonTextContinue = "Contine";
-std::string buttonTextSolo = "SoloFreePlayButton";
-std::string scenesNames[3] = {"ShaderWarmup", "HealthWarning", "MenuViewControllers"};
-bool timerOne = false;
-bool timerTwo = false;
-bool* timerOnePtr = &timerOne;
-bool* timerTwoPtr = &timerTwo;
 
-DECLARE_CLASS_CODEGEN(Il2CppNamespace, Timer, UnityEngine::MonoBehaviour,
+std::string scenesNames[3] = {"ShaderWarmup", "HealthWarning", "MenuViewControllers"};
+
+
+
+
+DECLARE_CLASS_CODEGEN(Il2CppNamespace, ButtonChecker, UnityEngine::MonoBehaviour,
     DECLARE_METHOD(void, Update);
-    REGISTER_FUNCTION(Timer,
+    REGISTER_FUNCTION(ButtonChecker,
         REGISTER_METHOD(Update);
     )
 )
 
 
-void Il2CppNamespace::Timer::Update() 
+void Il2CppNamespace::ButtonChecker::Update() 
 {
-  
+
 }
 
 
@@ -48,33 +46,18 @@ MAKE_HOOK_OFFSETLESS(ActiveSceneChanged, void, Scene prevScene, Scene nextScene)
    // Adding customtype CoroutineType to recreate the coroutine.
    if(sceneName == scenesNames[0])
    {
-     auto* gameObject =     UnityEngine::GameObject::New_ctor(il2cpp_utils::createcsstr("TimerGO"));
-     auto* component =      RET_V_UNLESS(il2cpp_utils::RunMethod(gameObject, "AddComponent", typeof(Il2CppNamespace::Timer*)));
+     auto* gameObject =     UnityEngine::GameObject::New_ctor(il2cpp_utils::createcsstr("ButtonCheckerGO"));
+     auto* component =      RET_V_UNLESS(il2cpp_utils::RunMethod(gameObject, "AddComponent", typeof(Il2CppNamespace::ButtonChecker*)));
      UnityEngine::Object::DontDestroyOnLoad(gameObject);
    }
 
 
 }
 
-MAKE_HOOK_OFFSETLESS(HealthWarningSceneStart_Start, void, Il2CppObject*self )
-{
-  HealthWarningSceneStart_Start(self);
-  std::thread buttonClick2(clickButton, "SoloFreePlayButton", foundButton2Ptr);
-	buttonClick2.detach();
-}
-
-MAKE_HOOK_OFFSETLESS(MenuSceneSetup_Start, void, Il2CppObject*self )
-{
-  MenuSceneSetup_Start(self);
-}
-
-void StartTimer(float seconds, bool* timerDone);
-
-
 extern "C" void setup(ModInfo& info) 
 {
     info.id = "JustTakeMeToTheSoloMode";
-    info.version = "0.0.1";
+    info.version = "0.1.0";
     modInfo = info;
     getLogger().info(info.version);
     getLogger().info(info.id);
@@ -82,8 +65,6 @@ extern "C" void setup(ModInfo& info)
 
 extern "C" void load() 
 {
-  custom_types::Register::RegisterType<Il2CppNamespace::Timer>();
+  custom_types::Register::RegisterType<Il2CppNamespace::ButtonChecker>();
   INSTALL_HOOK_OFFSETLESS(ActiveSceneChanged, il2cpp_utils::FindMethodUnsafe("UnityEngine.SceneManagement", "SceneManager", "Internal_ActiveSceneChanged", 2));
-  INSTALL_HOOK_OFFSETLESS(HealthWarningSceneStart_Start, il2cpp_utils::FindMethodUnsafe("", "HealthWarningSceneStart", "Start", 0));
-  INSTALL_HOOK_OFFSETLESS(MenuSceneSetup_Start, il2cpp_utils::FindMethodUnsafe("", "MenuSceneSetup", "Start", 0));
 }
