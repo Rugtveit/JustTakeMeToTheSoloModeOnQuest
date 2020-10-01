e5d1180681cd556864ea3e4e1ea0b3c82240809b
Rugtveit <BenjaminRugtveit@gmail.com>
2020-10-01 12:49:19 +0200

    Added Initialize

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@ const Logger& getLogger() {
   return logger;
 }
 
+// Declaring CustomType codegen class
 DECLARE_CLASS_CODEGEN(Il2CppNamespace, WaitUntilType, UnityEngine::MonoBehaviour,
     DECLARE_METHOD(void, Update);
     REGISTER_FUNCTION(WaitUntilType,
@@ -14,36 +15,46 @@ DECLARE_CLASS_CODEGEN(Il2CppNamespace, WaitUntilType, UnityEngine::MonoBehaviour
     )
 )
 
-ButtonFinder buttonFinder1; // Continue button finder
-ButtonFinder buttonFinder2; // SoloMode button finder
+ButtonFinder buttonFinder1; // Continue button
+ButtonFinder buttonFinder2; // SoloMode button 
 TabSelector tabSelector; 
 float waitingTime = 1.5f;
+
+void Initialize()
+{
+  buttonFinder1.buttonName = "Continue";
+  buttonFinder1.waitingTime = waitingTime;
+
+  buttonFinder2.buttonName = "SoloFreePlayButton";
+  buttonFinder2.waitingTime = waitingTime;
+
+  tabSelector.tab = 2;
+  tabSelector.waitTime = waitingTime;
+}
+
+std::string GetSceneName(Scene scene);
 
 MAKE_HOOK_OFFSETLESS(ActiveSceneChanged, void, Scene prevScene, Scene nextScene)
 {
   ActiveSceneChanged(prevScene, nextScene);
-  std::string nextSceneName = Utils::GetSceneName(nextScene);
+  std::string nextSceneName = GetSceneName(nextScene);
   if(nextSceneName == "") return; 
   
   if(nextSceneName == "ShaderWarmup")
   {
-    auto* gameObject =  UnityEngine::GameObject::New_ctor(il2cpp_utils::createcsstr("WaitUntilTypeGO"));
-    auto* component =   gameObject->AddComponent<Il2CppNamespace::WaitUntilType*>();
-    UnityEngine::Object::DontDestroyOnLoad(gameObject);
+    UnityEngine::GameObject* waitUntilGO =  UnityEngine::GameObject::New_ctor(il2cpp_utils::createcsstr("WaitUntilTypeGO"));
+    waitUntilGO->AddComponent<Il2CppNamespace::WaitUntilType*>();
+    UnityEngine::Object::DontDestroyOnLoad(waitUntilGO);
   }
 
   if(nextSceneName == "HealthWarning")
   {
-    buttonFinder1.buttonName = "Continue";
-    buttonFinder1.waitingTime = waitingTime;
     std::thread findContinueBtnThread(buttonFinder1.FindButton, buttonFinder1.waitingTime, buttonFinder1.foundButtonPtr, buttonFinder1.buttonName);
     findContinueBtnThread.detach();
   }
 
   if(nextSceneName == "MenuViewControllers")
   {
-    buttonFinder2.buttonName = "SoloFreePlayButton";
-    buttonFinder2.waitingTime = waitingTime;
     std::thread findSoloBtnThread(buttonFinder2.FindButton, buttonFinder2.waitingTime, buttonFinder2.foundButtonPtr, buttonFinder2.buttonName);
     findSoloBtnThread.detach();
   }
@@ -69,13 +80,13 @@ void Il2CppNamespace::WaitUntilType::Update()
   {
     UnityEngine::UI::Button* soloModeBtn = buttonFinder2.GetButton();
     soloModeBtn->Press();
-    tabSelector.tab = 1;
+ 
     if(tabSelector.IsValidTab())
     {
-      tabSelector.waitTime = waitingTime;
       std::thread waitThread(StartTimer, tabSelector.waitTime, tabSelector.waitDonePtr);
       waitThread.detach();
     }
+    
     buttonFinder2.pressedButton = true;
   }
 
@@ -85,7 +96,6 @@ void Il2CppNamespace::WaitUntilType::Update()
     tabSelector.tabSelected = true;
   }
 }
-
 
 extern "C" void setup(ModInfo& info) 
 {
@@ -98,6 +108,14 @@ extern "C" void setup(ModInfo& info)
 
 extern "C" void load() 
 {
+  Initialize();
   custom_types::Register::RegisterType<Il2CppNamespace::WaitUntilType>();
   INSTALL_HOOK_OFFSETLESS(ActiveSceneChanged, il2cpp_utils::FindMethodUnsafe("UnityEngine.SceneManagement", "SceneManager", "Internal_ActiveSceneChanged", 2));
+}
+
+std::string GetSceneName(Scene scene)
+{
+    Il2CppString* csString = RET_UNLESS("", il2cpp_utils::RunMethod<Il2CppString*>("UnityEngine.SceneManagement", "Scene", "GetNameInternal", scene.m_Handle));
+    RET_UNLESS("", csString);
+    return to_utf8(csstrtostr(csString));
 }
